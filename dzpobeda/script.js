d13bff6a7ce07f96a82005f2c9dca13cba0c4768
brawlstars2005GHOUL <124290393+brawlstars2005GHOUL@users.noreply.github.com>
2023-02-02 23:08:10 +0600

    Add files via upload

---
+++ dzpobeda/script.js
@@ -0,0 +1,25 @@
+function openModal(){
+    let layer = document.getElementById("layer");
+    let modal = document.getElementById("modal");
+    layer.style.display = "block"
+    modal.style.display = "block"
+}
+function closeModal(){
+    let layer = document.getElementById("layer");
+    let modal = document.getElementById("modal");
+    layer.style.display = "none"
+    modal.style.display = "none"
+}
+
+function openModal2(){
+    let layer = document.getElementById("layer2");
+    let modal = document.getElementById("modal2");
+    layer.style.display = "block"
+    modal.style.display = "block"
+}
+function closeModal2(){
+    let layer = document.getElementById("layer2");
+    let modal = document.getElementById("modal2");
+    layer.style.display = "none"
+    modal.style.display = "none"
+}
